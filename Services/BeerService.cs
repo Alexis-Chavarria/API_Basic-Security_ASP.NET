3cbc3044f7c9e6f153e91ee6cba7106baa351112
Johan-Chavarria <johan_chavarria@cun.edu.co>
2023-10-04 17:27:25 -0500

    Añado clase 'BeerService' e implemento método que permitira leer el JSON con la data de las cerveza

---
+++ Services/BeerService.cs
@@ -0,0 +1,12 @@
+﻿using API_Basic_Security_ASP.Models;
+
+namespace API_Basic_Security_ASP.Services
+{
+    public class BeerService : IBeerService
+    {
+        public Task<List<Beer>> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Services/IBeerService.cs
+++ Services/IBeerService.cs
@@ -1,6 +1,9 @@
-﻿namespace API_Basic_Security_ASP.Services
+﻿using API_Basic_Security_ASP.Models;
+
+namespace API_Basic_Security_ASP.Services
 {
     public interface IBeerService
     {
+        public Task<List<Beer>> Get();
     }
 }
