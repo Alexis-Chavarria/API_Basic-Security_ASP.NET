cad7fbb7da7be1d664e656de7c93733b5d694afd
Johan-Chavarria <johan_chavarria@cun.edu.co>
2023-10-05 09:51:53 -0500

    Se crea entidad 'User' junto a su capa de servicio

---
+++ Services/UserService.cs
@@ -0,0 +1,19 @@
+﻿using API_Basic_Security_ASP.Models;
+
+namespace API_Basic_Security_ASP.Services
+{
+    public class UserService : IUserService
+    {
+        List<User> users = new List<User>()
+        {
+            new User(){ email = "alexis@gmail.com", password = "123"},
+            new User(){ email = "johan@gmail.com", password = "123"}
+        };
+
+        public bool IsUser(string email, string password) =>
+            users.Where(d => d.email == email && d.password == password).Count() > 0;
+
+
+
+    }
+}
