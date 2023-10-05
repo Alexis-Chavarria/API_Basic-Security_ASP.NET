c9868a3e00805f209a32fe28e919130560e9cfa4
Johan-Chavarria <johan_chavarria@cun.edu.co>
2023-10-05 09:17:05 -0500

    Se crea capa del controlador junto a EndPoint GET, sin embargo, el JSON recibido llega null

---
+++ Controllers/BeerController.cs
@@ -0,0 +1,22 @@
+﻿using API_Basic_Security_ASP.Models;
+using API_Basic_Security_ASP.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Basic_Security_ASP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeerController : ControllerBase
+    {
+        private readonly IBeerService _beerService;
+
+        public BeerController(IBeerService beerService)
+        {
+            this._beerService = beerService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Beer>> Get() => await _beerService.Get();
+    }
+}
--- Services/BeerService.cs
+++ Services/BeerService.cs
@@ -1,12 +1,17 @@
 ﻿using API_Basic_Security_ASP.Models;
+using System.Text.Json;
 
 namespace API_Basic_Security_ASP.Services
 {
     public class BeerService : IBeerService
     {
-        public Task<List<Beer>> Get()
+        string path = @"C:\Users\CUN\Documents\CUN\Desarrollo\Proyectos\APIs\API_Basic-Security_ASP\API_Basic-Security_ASP\beers.json";
+        public async Task<List<Beer>> Get()
         {
-            throw new NotImplementedException();
+            string content = await File.ReadAllTextAsync(path);
+            var beers = JsonSerializer.Deserialize<List<Beer>>(content);
+
+            return beers;
         }
     }
 }
