c1fd7e61a126ae48f8bf54a96f728bc79d3cbcc6
Johan-Chavarria <johan_chavarria@cun.edu.co>
2023-10-05 14:29:50 -0500

    Guardar cambios en la rama main

--- Services/BeerService.cs
+++ Services/BeerService.cs
@@ -5,7 +5,7 @@ namespace API_Basic_Security_ASP.Services
 {
     public class BeerService : IBeerService
     {
-        string path = @"C:\Users\CUN\Documents\CUN\Desarrollo\Proyectos\APIs\API_Basic-Security_ASP\API_Basic-Security_ASP\beers.json";
+        string path = @"C:\Users\CUN\Documents\CUN\Desarrollo\Proyectos\APIs\API_Basic-Security_ASP.NET\API_Basic-Security_ASP.NET\beers.json";
         public async Task<List<Beer>> Get()
         {
             string content = await File.ReadAllTextAsync(path);
